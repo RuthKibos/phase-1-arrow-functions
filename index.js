2fb592e5fcddcef9a88f6b882f1912c557a207da
RuthKibos <kibosruth@gmail.com>
2024-03-22 23:34:30 +0300

    Completed the Lab

---
+++ index.js
@@ -0,0 +1,6 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2+3);
+const divide =(parameter1, parameter2) => parameter1 / parameter2;
+divide(8/4);
+const square =(parameter) => parameter ** parameter;
+square(4*4);
